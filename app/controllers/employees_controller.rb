98c1da764a7c893678addea834562265c3d3f663
pranavwaikar <pranavwaikar@gmail.com>
2020-01-21 16:21:05 +0530

    validations & controllers

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -2,4 +2,10 @@ class EmployeesController < ApplicationController
   def index
     @employees=Employee.all
   end
+  
+  def new
+  end
+
+  def create
+  end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,6 @@
 class Employee < ActiveRecord::Base
+  validates :first_name,  presence:true, length: {minimum:3 , maximum:20}
+  validates :last_name, presence:true, length: {minimum:3, maximum:20}
+  validates :company_name, presence:true, length: {minimum:3, maximum:50}
+  validates :designation, presence:true, length:{minimum:5, maximum:30}
 end
